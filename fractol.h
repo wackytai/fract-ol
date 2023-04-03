5d2deff838d43e0939ad0b526dae6cfa5a42130d
Tai Monteiro <48991630+wackytai@users.noreply.github.com>
2023-04-03 15:26:19 +0100

    Data Struct + #1 Window

--- fractol.h
+++ fractol.h
@@ -6,20 +6,29 @@
 /*   By: tlemos-m <tlemos-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/03 14:07:25 by tlemos-m          #+#    #+#             */
-/*   Updated: 2023/04/03 14:34:36 by tlemos-m         ###   ########.fr       */
+/*   Updated: 2023/04/03 15:08:40 by tlemos-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FRACTOL_H
 # define FRACTOL_H
-# ifndef WINDOW_WIDTH
-#  define WINDOW_WIDTH 1920
+# ifndef W_WIDTH
+#  define W_WIDTH 1920
 # endif
 
-# ifndef WINDOW_HEIGHT
-#  define WINDOW_HEIGHT 1080
+# ifndef W_HEIGHT
+#  define W_HEIGHT 1080
 # endif
+
+typedef struct s_data
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+}	t_data;
 
 /*main.c*/
+int	main(void);
+int	handle_input(int keysym, t_data *data);
+int	handle_no_event(void *data);
 
 #endif
--- main.c
+++ main.c
@@ -6,31 +6,46 @@
 /*   By: tlemos-m <tlemos-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/03 12:44:06 by tlemos-m          #+#    #+#             */
-/*   Updated: 2023/04/03 14:44:38 by tlemos-m         ###   ########.fr       */
+/*   Updated: 2023/04/03 15:10:44 by tlemos-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include <X11/keysym.h>
 #include "fractol.h"
 #include "./mlx_linux/mlx.h"
 
+int	handle_no_event(void *data)
+{
+	if (data)
+		return (0);
+	return (0);
+}
+
+int	handle_input(int keysym, t_data *data)
+{
+	if (keysym == XK_Escape)
+		mlx_destroy_window(data->mlx_ptr, data->win_ptr);
+	return (0);
+}
+
 int	main(void)
 {
-	void	*mlx_ptr;
-	void	*win_ptr;
+	t_data	data;
 
-	mlx_ptr = mlx_init();
-	if (!mlx_ptr)
+	data.mlx_ptr = mlx_init();
+	if (!data.mlx_ptr)
 		return (1);
-	win_ptr = mlx_new_window(mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT, "fract-ol");
-	if (!win_ptr)
+	data.win_ptr = mlx_new_window(data.mlx_ptr, W_WIDTH, W_HEIGHT, "fract-ol");
+	if (!data.win_ptr)
 	{
-		free(mlx_ptr);
+		free(data.mlx_ptr);
 		return (1);
 	}
-	while (1)
-		;
-	mlx_destroy_window(mlx_ptr, win_ptr);
-	mlx_destroy_display(mlx_ptr);
-	free(mlx_ptr);
+	mlx_loop_hook(data.mlx_ptr, &handle_no_event, &data);
+	mlx_key_hook(data.win_ptr, &handle_input, &data);
+	mlx_loop(data.mlx_ptr);
+	//mlx_destroy_window(data.mlx_ptr, data.win_ptr);
+	mlx_destroy_display(data.mlx_ptr);
+	free(data.mlx_ptr);
 }
