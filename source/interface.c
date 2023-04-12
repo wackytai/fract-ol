6f65f540a4ef2f4e7661f63fd07698cc39c3312d
wackytai <tainahmonteiro.info@gmail.com>
2023-04-12 15:59:42 +0100

    Help Menu to Test

--- source/interface.c
+++ source/interface.c
@@ -6,7 +6,7 @@
 /*   By: tlemos-m <tlemos-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/12 15:16:50 by tlemos-m          #+#    #+#             */
-/*   Updated: 2023/04/12 15:31:42 by tlemos-m         ###   ########.fr       */
+/*   Updated: 2023/04/12 15:58:32 by tlemos-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 //update_zoom must receive the struct which has the zoom variable as a member
 //replace int zoom for the struct member when struct is ready
+//don't forget to make zoom relative to mouse position
 void	update_zoom(void)
 {
 	int	zoom;
@@ -28,8 +29,17 @@ void	update_zoom(void)
 
 int	create_menu(t_data *data)
 {
-	render_background(&data->img, 0x000000);
+	render_background(&data->img, 0x808080);
 	mlx_string_put(data->mlx_ptr, data->win_ptr, W_WIDTH / 2,
-		W_HEIGHT / 3, 0xFFFFFF, "Help!");
+		W_HEIGHT / 4, 0xFFFFFF, "Help Center");
+	mlx_string_put(data->mlx_ptr, data->win_ptr, W_WIDTH / 3,
+		(W_HEIGHT / 3) * 2, 0xFFFFFF,
+		"Fractals\nAvailable:");
+	mlx_string_put(data->mlx_ptr, data->win_ptr, (W_WIDTH / 3) * 2,
+		(W_HEIGHT / 3) * 2, 0xFFFFFF, "Mandelbrot\nJulia\nCantor Set");
+	mlx_string_put(data->mlx_ptr, data->win_ptr, W_WIDTH / 3,
+		W_HEIGHT - W_HEIGHT / 4, 0xFFFFFF, "Zoom:\nPan:\nColour Range Shift:");
+	mlx_string_put(data->mlx_ptr, data->win_ptr, (W_WIDTH / 3) * 2,
+		W_HEIGHT - W_HEIGHT / 4, 0xFFFFFF, "Mouse Scroll\nArrows\nMouse Move");
 	return (0);
 }
