bdb44e3774c620cfd0e17d3ec952fe8523a4a676
wackytai <tainahmonteiro.info@gmail.com>
2023-04-20 13:28:25 +0100

    Zoom Variable

--- source/clean.c
+++ source/clean.c
@@ -6,7 +6,7 @@
 /*   By: tlemos-m <tlemos-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/10 15:53:35 by tlemos-m          #+#    #+#             */
-/*   Updated: 2023/04/20 11:26:50 by tlemos-m         ###   ########.fr       */
+/*   Updated: 2023/04/20 13:17:06 by tlemos-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,6 @@ int	destroy_all(t_data *data)
 {
 	mlx_destroy_image(data->mlx_ptr, data->img.mlx_img);
 	mlx_destroy_display(data->mlx_ptr);
-	/* while (data->img.img_map[++x])
-		free(data->img.img_map[x]);
-	free(data->img.img_map); */
 	return (0);
 }
 
--- source/init.c
+++ source/init.c
@@ -6,7 +6,7 @@
 /*   By: tlemos-m <tlemos-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/14 13:49:36 by tlemos-m          #+#    #+#             */
-/*   Updated: 2023/04/20 12:59:30 by tlemos-m         ###   ########.fr       */
+/*   Updated: 2023/04/20 13:24:41 by tlemos-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,6 @@ int	initialize(int argc, char **argv, t_data *data, t_fractal *fractal)
 		data->img.mlx_img = mlx_new_image(data->mlx_ptr, W_WIDTH, W_HEIGHT);
 		data->img.addr = mlx_get_data_addr(data->img.mlx_img, &data->img.bpp,
 				&data->img.line_len, &data->img.endian);
-		/* data->img.img_map = (int **)ft_calloc(sizeof(int *), W_HEIGHT + 1);
-		while (++x < W_HEIGHT)
-			data->img.img_map[x] = (int *)ft_calloc(sizeof(int), W_WIDTH); */
 	}
 	if (argc == 1)
 	{
@@ -64,12 +61,12 @@ void	set_struct(t_fractal *fractal)
 {
 	fractal->pixel.x = 0;
 	fractal->pixel.y = 0;
-	fractal->f_center.x = -0.75;
-	fractal->f_center.y = -1;
-	fractal->max.x = 2;
-	fractal->max.y = 2;
-	fractal->min.x = -1.5;
-	fractal->min.y = 0;
+	fractal->f_center.x = -0.75 * fractal->zoom;
+	fractal->f_center.y = -1 * fractal->zoom;
+	fractal->max.x = 2 * fractal->zoom;
+	fractal->max.y = 2 * fractal->zoom;
+	fractal->min.x = -1.5 * fractal->zoom;
+	fractal->min.y = 0 * fractal->zoom;
 	fractal->offset.x = 0;
 	fractal->offset.y = 0;
 	fractal->zoom = 1;
