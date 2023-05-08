ae712dbbf10a0a563cf0173b8ccd984e786acfe6
wackytai <tainahmonteiro.info@gmail.com>
2023-05-08 10:03:30 +0100

    Zoom Fine Tuning

--- source/render_utils.c
+++ source/render_utils.c
@@ -6,23 +6,32 @@
 /*   By: tlemos-m <tlemos-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/20 13:52:14 by tlemos-m          #+#    #+#             */
-/*   Updated: 2023/05/05 11:28:51 by tlemos-m         ###   ########.fr       */
+/*   Updated: 2023/05/08 10:02:13 by tlemos-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/fractol.h"
 
-//don't forget to make zoom relative to mouse position
 void	update_zoom(t_data *data, int x, int y)
 {
 	if (x <= W_WIDTH && y <= W_HEIGHT)
 	{
 		if (data->f->z_flag > 0)
-			data->f->zoom *= 1.15;
+		{
+			data->f->zoom *= 1.05;
+			data->f->offset.x -= (((double)x - (W_WIDTH / 2)) / W_WIDTH)
+				* data->f->zoom;
+			data->f->offset.y -= (((double)y - (W_HEIGHT / 2)) / W_HEIGHT)
+				* data->f->zoom;
+		}
 		else if (data->f->z_flag < 0)
-			data->f->zoom *= 0.85;
-		data->f->offset.x += (((double)x - (W_WIDTH / 2)) / W_WIDTH) * data->f->zoom;
-		data->f->offset.y += (((double)y - (W_HEIGHT / 2)) / W_HEIGHT) * data->f->zoom;
+		{
+			data->f->zoom *= 0.95;
+			data->f->offset.x += (((double)x - (W_WIDTH / 2)) / W_WIDTH)
+				* data->f->zoom;
+			data->f->offset.y += (((double)y - (W_HEIGHT / 2)) / W_HEIGHT)
+				* data->f->zoom;
+		}
 		update_screen(data);
 	}
 	return ;
@@ -31,13 +40,13 @@ void	update_zoom(t_data *data, int x, int y)
 void	pan_image(t_data *data, int flag)
 {
 	if (flag == 1)
-		data->f->offset.x += 0.5 * data->f->zoom;
+		data->f->offset.x += 0.15 * data->f->zoom;
 	else if (flag == -1)
-		data->f->offset.x -= 0.5 * data->f->zoom;
+		data->f->offset.x -= 0.15 * data->f->zoom;
 	else if (flag == 2)
-		data->f->offset.y -= 0.5 * data->f->zoom;
+		data->f->offset.y -= 0.15 * data->f->zoom;
 	else if (flag == -2)
-		data->f->offset.y += 0.5 * data->f->zoom;
+		data->f->offset.y += 0.15 * data->f->zoom;
 	update_screen(data);
 	return ;
 }
